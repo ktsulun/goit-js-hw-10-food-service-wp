b8f52693dd7cc52f498804e2d1ebb21ce34a390d
ktsulun <ktsulun@gmail.com>
2020-11-16 19:00:41 +0200

    commit tested

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './styles.css';
 import menuCardsTpl from './menu.hbs';
 import menu from './menu.json';
 
-
 const themeSwitchToggler = document.querySelector('#theme-switch-toggle');
 const menuContainer = document.querySelector('.js-menu');
 const body = document.body;
@@ -16,36 +15,35 @@ themeSwitchToggler.addEventListener('change', themeSwitcher);
 
 themeSwitchToggler.checked = false;
 function themeSwitcher() {
-    if (this.checked) {
-        body.classList.remove('light-theme');
-        body.classList.add('dark-theme');
-        localStorage.setItem(THEME_KEY, 'DARK');
-    } else {
-        body.classList.add('light-theme');
-        body.classList.remove('dark-theme');
-        localStorage.setItem(THEME_KEY, 'LIGHT');
-    }
+  if (this.checked) {
+    body.classList.remove('light-theme');
+    body.classList.add('dark-theme');
+    localStorage.setItem(THEME_KEY, 'DARK');
+  } else {
+    body.classList.add('light-theme');
+    body.classList.remove('dark-theme');
+    localStorage.setItem(THEME_KEY, 'LIGHT');
+  }
 }
 
 window.onload = checkLocalStorageTheme;
 
 function checkLocalStorageTheme() {
-    const localStorageTheme = localStorage.getItem(THEME_KEY);
+  const localStorageTheme = localStorage.getItem(THEME_KEY);
 
-    if (localStorageTheme === null || localStorageTheme === 'LIGHT') {
-        themeSwitchToggler.checked = false;
-    } else {
-        body.classList.remove('light-theme');
-        body.classList.add('dark-theme');
-        themeSwitchToggler.checked = true;
-    }
+  if (localStorageTheme === null || localStorageTheme === 'LIGHT') {
+    themeSwitchToggler.checked = false;
+  } else {
+    body.classList.remove('light-theme');
+    body.classList.add('dark-theme');
+    themeSwitchToggler.checked = true;
+  }
 }
-
 
 const cardsMarkup = menuCardsCreator(menu);
 
 function menuCardsCreator(menu) {
-    return menuCardsTpl(menu);
+  return menuCardsTpl(menu);
 }
 
 menuContainer.insertAdjacentHTML('beforeend', cardsMarkup);
